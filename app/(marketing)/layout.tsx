6e041113d141d517ca1305367b3592e7cff99290
sinergetic <kelly.g.villalobos@gmail.com>
2026-06-25 16:40:35 -0400

    Fix Vercel dynamic rendering build issues

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,6 +5,8 @@ import { Suspense } from 'react'
 import DashboardButton from '../components/DashboardButton'
 import Button from '../components/ui/Button'
 
+export const dynamic = 'force-dynamic'
+
 export default async function MarketingLayout({
   children,
 }: {
